5aec7662448eb39c6030bf8234d511b7d20b2e4c
xukai <xukai@multicorewareinc.com>
2015-05-15 11:50:23 +0800

    add:adding for absval_layer.cu

--- src/caffe/util/math_functions_amp.cxx
+++ src/caffe/util/math_functions_amp.cxx
@@ -7,32 +7,45 @@ using namespace concurrency;
 namespace caffe {
 
 #ifdef USE_CPPAMP
-template <>
-void caffe_amp_abs<float>(const int n, float* a, float* y) {
-  array_view<float, 1> aView(n, a);
-  array_view<float, 1> yView(n, y);
+template <typename Dtype>
+void caffe_amp_abs(const int N, Dtype* a, Dtype* y) {
+  array_view<Dtype, 1> aView(N, a);
+  array_view<Dtype, 1> yView(N, y);
   parallel_for_each(
-    yView.get_extent(),
+    yView.extent,
     [=](index<1> idx) restrict(amp)
   {
-    yView[idx] = Concurrency::fast_math::fabs(aView[idx]);
+    yView[idx] = aView[idx] >= 0 ? aView[idx] : -1 * aView[idx];
+  }
+  );
+}                                                                                                                                                
+
+template <typename Dtype>
+void caffe_amp_sign(const int N, Dtype* a, Dtype* y) {
+  array_view<Dtype, 1> aView(N, a);
+  array_view<Dtype, 1> yView(N, y);
+  parallel_for_each(
+    yView.extent,
+    [=](index<1> idx) restrict(amp)
+  {
+    yView[idx] = aView[idx] == 0 ? 0 : (aView[idx] < 0 ? -1 : 1);
   }
   );
 }
-template <>
-void caffe_amp_mul<float>(const int n, const float* a, const float* b, float* y){
-  /*array_view<float, 1> aView(N, a);
-  array_view<float, 1> bView(N, b);
-  array_view<float, 1> yView(N, y);
+
+template <typename Dtype>
+void caffe_amp_mul(const int N, Dtype* a, Dtype* b, Dtype* y) {
+  array_view<Dtype, 1> aView(N, a);
+  array_view<Dtype, 1> bView(N, b);
+  array_view<Dtype, 1> yView(N, y);
   parallel_for_each(
-    yView.get_extent(),
+    yView.extent,
     [=](index<1> idx) restrict(amp)
   {
     yView[idx] = (aView[idx] * bView[idx]);
   }
-  );*/
+  );
 }
-
 template <typename Dtype>
 void set_kernel(const int N, const Dtype alpha, Dtype* y) {
   array_view<Dtype, 1> outView(N, y);
