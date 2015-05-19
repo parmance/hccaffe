45d5e94904f28bcc80054f5c197232a7fc8d618a
Dongji Yi <dongji@multicorewareinc.com>
2015-05-19 14:38:21 +0800

    Update add bnll_layer_amp.cxx

--- src/caffe/layers/bnll_layer_amp.cxx
+++ src/caffe/layers/bnll_layer_amp.cxx
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <vector>
 #include <amp.h>
+#include <amp_math.h>
 #include "caffe/layer.hpp"
 #include "caffe/vision_layers.hpp"
 using namespace Concurrency;
@@ -9,41 +10,41 @@ namespace caffe {
 	const float kBNLL_THRESHOLD = 50.;
 
 	template <typename Dtype>
-	void BNLLForward(const int n, const Dtype* in, Dtype* out) {
+	void BNLLForward(const int n,  Dtype* in, Dtype* out) {
 
 		array_view<Dtype, 1> inView(n, in);
 		array_view<Dtype, 1> outView(n, out);
 		parallel_for_each(
-			yView.get_extent(),
+			outView.get_extent(),
 			[=](index<1> idx) restrict(amp)
 		{
 			outView[idx] = inView[idx] > 0 ?
-				inView[idx] + log(1. + exp(-inView[idx])) :
-				log(1. + exp(inView[idx]));
+				inView[idx] + fast_math::log(1. + fast_math::exp(-inView[idx])) :
+				fast_math::log(1. + fast_math::exp(inView[idx]));
 		}
 		);
 	}
 	template <typename Dtype>
 	void BNLLLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom,
 		const vector<Blob<Dtype>*>& top) {
-	    Dtype* bottom_data = const_cast <Dtype*>(bottom[0]->gpu_data()£©;
-		Dtype* top_data = const_cast <Dtype*>(top[0]->mutable_gpu_data()£©;
+	    Dtype* bottom_data = const_cast <Dtype*>(bottom[0]->gpu_data());
+		Dtype* top_data = const_cast <Dtype*>(top[0]->mutable_gpu_data());
 		const int count = bottom[0]->count();
 		// NOLINT_NEXT_LINE(whitespace/operators)
 		BNLLForward(count, bottom_data, top_data);
 	}
 
 	template <typename Dtype>
-	void BNLLBackward(const int n, const Dtype* in_diff,
-		const Dtype* in_data, Dtype* out_diff) {
+	void BNLLBackward(const int n,  Dtype* in_diff,
+		 Dtype* in_data, Dtype* out_diff) {
 		array_view<Dtype, 1> inDiffView(n, in_diff);
 		array_view<Dtype, 1> inDataView(n, in_data);
 		array_view<Dtype, 1> outDiffView(n, out_diff);
 		parallel_for_each(
-			yView.get_extent(),
+			outDiffView.get_extent(),
 			[=](index<1> idx) restrict(amp)
 		{
-			Dtype expval = exp(min(inDataView[idx], Dtype(kBNLL_THRESHOLD)));
+			Dtype expval = fast_math::exp(min(inDataView[idx], Dtype(kBNLL_THRESHOLD)));
 			outDiffView[idx] = inDiffView[idx] * expval / (expval + 1.);
 		}
 		);
