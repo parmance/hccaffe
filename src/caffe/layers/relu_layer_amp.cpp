4ac03baae5237a27fc15b70e2f2cb2fa78531068
xukai <xukai@multicorewareinc.com>
2015-05-25 16:45:33 +0800

    update:updating for relu_layer.cxx

--- src/caffe/layers/relu_layer_amp.cpp
+++ src/caffe/layers/relu_layer_amp.cpp
@@ -3,42 +3,18 @@
 
 #include "caffe/layer.hpp"
 #include "caffe/vision_layers.hpp"
-#include "amp.h"
-#include "amp_math.h"
 
-using namespace Concurrency;
-namespace caffe {
+#ifdef USE_CPPAMP
 
 template <typename Dtype>
 void ReLUForward(const int N, Dtype* in, Dtype* out,
-  Dtype negative_slope) {
-  array_view<Dtype, 1> inView(N, in);
-  array_view<Dtype, 1> outView(N, out);
-  parallel_for_each(
-    outView.get_extent(),
-    [=](index<1> idx) restrict(amp)
-  {
-    outView[idx] = inView[idx] > 0 ? inView[idx] : inView[idx] * negative_slope;
-  }
-  );
-  outView.synchronize();
-}
+  Dtype negative_slope);
 
 template <typename Dtype>
 void ReLUBackward(const int N, Dtype* in_diff,
-  Dtype* in_data, Dtype* out_diff, Dtype negative_slope) {
-  array_view<Dtype, 1> in_diffView(N, in_diff);
-  array_view<Dtype, 1> out_diffView(N, out_diff);
-  array_view<Dtype, 1> int_dataView(N, in_data);
-  parallel_for_each(
-    out_diffView.get_extent(),
-    [=](index<1> idx) restrict(amp)
-  {
-    out_diffView[idx] = in_diffView[idx] * ((int_dataView[idx] > 0) + (int_dataView[idx] <= 0) * negative_slope);
-  }
-  );
-  out_diffView.synchronize();
-}
+  Dtype* in_data, Dtype* out_diff, Dtype negative_slope);
+
+namespace caffe {
 
 template <typename Dtype>
 void ReLULayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom,
@@ -70,3 +46,4 @@ INSTANTIATE_LAYER_GPU_FUNCS(ReLULayer);
 
 
 }  // namespace caffe
+#endif
