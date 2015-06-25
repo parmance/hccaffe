092f80cdabd5bfaeec4a1329a67ec14b37f4780f
haochen <haochen@multicorewareinc.com>
2015-06-25 13:37:48 +0800

    Fix bug of deconv_layer with C++AMP code

--- src/caffe/layers/deconv_layer_amp.cpp
+++ src/caffe/layers/deconv_layer_amp.cpp
@@ -11,13 +11,13 @@ namespace caffe {
 template <typename Dtype>
 void DeconvolutionLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top) {
-/*
+
   const Dtype* weight = this->blobs_[0]->gpu_data();
   for (int i = 0; i < bottom.size(); ++i) {
     const Dtype* bottom_data = bottom[i]->gpu_data();
     Dtype* top_data = top[i]->mutable_gpu_data();
     for (int n = 0; n < this->num_; ++n) {
-      this->backward_gpu_gemm(bottom_data + bottom[i]->offset(n), weight,
+      this->backward_gpu_gemm2(bottom[i]->count()-bottom[i]->offset(n),top[i]->count()-top[i]->offset(n),bottom_data + bottom[i]->offset(n), weight,
           top_data + top[i]->offset(n));
       if (this->bias_term_) {
         const Dtype* bias = this->blobs_[1]->gpu_data();
@@ -25,13 +25,13 @@ void DeconvolutionLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom,
       }
     }
   }
-*/
+
 }
 
 template <typename Dtype>
 void DeconvolutionLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>& top,
       const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
-/*
+
   const Dtype* weight = this->blobs_[0]->gpu_data();
   Dtype* weight_diff = this->blobs_[0]->mutable_gpu_diff();
   if (this->param_propagate_down_[0]) {
@@ -56,17 +56,17 @@ void DeconvolutionLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>& top,
       for (int n = 0; n < this->num_; ++n) {
         // gradient w.r.t. weight. Note that we will accumulate diffs.
         if (this->param_propagate_down_[0]) {
-          this->weight_gpu_gemm(top_diff + top[i]->offset(n),
+          this->weight_gpu_gemm2(top[i]->count()-top[i]->offset(n),bottom[i]->count()-bottom[i]->offset(n),top_diff + top[i]->offset(n),
               bottom_data + bottom[i]->offset(n), weight_diff);
         }
         // gradient w.r.t. bottom data, if necessary.
         if (propagate_down[i]) {
-          this->forward_gpu_gemm(top_diff + top[i]->offset(n), weight,
+          this->forward_gpu_gemm2(top[i]->count()-top[i]->offset(n),bottom[i]->count()-bottom[i]->offset(n),top_diff + top[i]->offset(n), weight,
               bottom_diff + bottom[i]->offset(n));
         }
       }
     }
-  }*/
+  }
 }
 
 INSTANTIATE_LAYER_GPU_FUNCS(DeconvolutionLayer);
