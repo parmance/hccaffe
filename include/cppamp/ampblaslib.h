c556e172e27ff73530f19c1875df295a7db3e7c0
dongji <dongji@multicorewareinc.com>
2015-07-01 11:47:42 +0800

    Update ampblas header file, add function declaration.

--- include/cppamp/ampblaslib.h
+++ include/cppamp/ampblaslib.h
@@ -16,6 +16,31 @@
 #include "amp_math.h"
 using namespace concurrency;
 
+/* enumerator to indicate the status of  blas operation */
+enum ampblasStatus {
+    AMPBLAS_SUCCESS = 0,
+    AMPBLAS_INVALID = -1,
+    AMPBLAS_ERROR = -2
+};
+
+/* enumerator to define the layout of  input matrix for blas operation */
+enum AMPBLAS_ORDER {
+    rowMajor ,
+    colMajor
+};
+
+/* enumerator to define the type of operation to be performed on the input matrix
+ ( NO_TRANSPOSE, TRANSPOSE, CONJUGATE) */
+enum AMPBLAS_TRANS {
+    noTrans = 'n',
+    trans = 't',
+    conjugate = 'c'
+};
+struct ampComplex
+{
+     float real;
+     float img;
+};
 template <typename Dtype>
 void amp_axpy(const int N, const Dtype alpha, Dtype* x, Dtype* y)
 {
@@ -60,27 +85,36 @@ void amp_scale(const int N, const Dtype value, Dtype* x)
   xView.synchronize();
 }
 
-/*
-void amp_sgemv(const enum CBLAS_TRANSPOSE type,
-  const int M, const int N,
-  const float alpha, float *A, const int N,
-  float *X, const long xOffset,
-  const float beta,
-  float *Y,const long yOffset)
-{
-  return;
-}
+/*                  Y = alpha * op(A) * X + beta * Y                     */
+ampblasStatus  ampblas_sgemv(const enum AMPBLAS_TRANS type, const int M,
+                             const int N, const float *alpha, float *A,
+                             const long aOffset,const int lda, float *X,
+                             const long xOffset, const int incX,
+                             const float *beta, float *Y,const long yOffset,
+                             const int incY);
 
-void amp_dgemv(const enum CBLAS_TRANSPOSE type,
-  const int M, const int N,
-  const double alpha, double *A, const int N,
-  double *X, const long xOffset,
-  const float beta,
-  double *Y,const long yOffset)
-{
-  return;
-}
-*/
+ampblasStatus  ampblas_dgemv(const enum AMPBLAS_TRANS type, const int M,
+                             const int N, const double *alpha, double *A,
+                             const long aOffset,const int lda, double *X,
+                             const long xOffset, const int incX,
+                             const double *beta, double *Y,const long yOffset,
+                             const int incY);
 
+/*                  C = alpha * op(A) * op(B) + beta * C                 */
+ampblasStatus  ampblas_sgemm(const enum AMPBLAS_TRANS typeA,
+                             const enum AMPBLAS_TRANS typeB, const int M,
+                             const int N, const int K, const float *alpha,
+                             float *A, const long lda, float *B,
+                             const long ldb, const float *beta, float *C,
+                             const long ldc, const long aOffset,
+                             const long bOffset, const long cOffset);
+
+ampblasStatus  ampblas_dgemm(const enum AMPBLAS_TRANS typeA,
+                             const enum AMPBLAS_TRANS typeB, const int M,
+                             const int N, const int K, const double *alpha,
+                             double *A, const long lda, double *B,
+                             const long ldb, const double *beta, double *C,
+                             const long ldc, const long aOffset,
+                             const long bOffset, const long cOffset);
 
 #endif
