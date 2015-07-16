015ef2fa912a021b42c4bad5561a8890983cabb2
Yang Zhang <yangzhang@multicorewareinc.com>
2015-07-16 09:33:38 +0800

    Rollback softmax_loss_layer
    The AMP version of softmax_loss_layer don't make loss became small,
    so rollback it.

--- src/caffe/layers/softmax_loss_layer_amp.cpp
+++ src/caffe/layers/softmax_loss_layer_amp.cpp
@@ -22,7 +22,35 @@ namespace caffe {
 template <typename Dtype>
 void SoftmaxWithLossLayer<Dtype>::Forward_gpu(
     const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top) {
-    softmax_layer_->Forward(softmax_bottom_vec_, softmax_top_vec_);
+      // The forward pass computes the softmax prob values.
+  softmax_layer_->Forward(softmax_bottom_vec_, softmax_top_vec_);
+  const Dtype* prob_data = prob_.cpu_data();
+  const Dtype* label = bottom[1]->cpu_data();
+  int dim = prob_.count() / outer_num_;
+  int count = 0;
+  Dtype loss = 0;
+  for (int i = 0; i < outer_num_; ++i) {
+    for (int j = 0; j < inner_num_; j++) {
+      const int label_value = static_cast<int>(label[i * inner_num_ + j]);
+      if (has_ignore_label_ && label_value == ignore_label_) {
+        continue;
+      }
+      DCHECK_GE(label_value, 0);
+      DCHECK_LT(label_value, prob_.shape(softmax_axis_));
+      loss -= log(std::max(prob_data[i * dim + label_value * inner_num_ + j],
+                           Dtype(FLT_MIN)));
+      ++count;
+    }
+  }
+  if (normalize_) {
+    top[0]->mutable_cpu_data()[0] = loss / count;
+  } else {
+    top[0]->mutable_cpu_data()[0] = loss / outer_num_;
+  }
+  if (top.size() == 2) {
+    top[1]->ShareData(prob_);
+  }
+  /*softmax_layer_->Forward(softmax_bottom_vec_, softmax_top_vec_);
     Dtype* prob_data = const_cast <Dtype*>(prob_.gpu_data());
     Dtype* label = const_cast <Dtype*>(bottom[1]->gpu_data());
     const int dim = prob_.count() / outer_num_;
@@ -50,13 +78,45 @@ void SoftmaxWithLossLayer<Dtype>::Forward_gpu(
     top[0]->mutable_cpu_data()[0] = loss;
     if (top.size() == 2) {
         top[1]->ShareData(prob_);
-    }
+    }*/
  
 }
 template <typename Dtype>
 void SoftmaxWithLossLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>& top,
         const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
-    if (propagate_down[1]) {
+     if (propagate_down[1]) {
+    LOG(FATAL) << this->type()
+               << " Layer cannot backpropagate to label inputs.";
+  }
+  if (propagate_down[0]) {
+    Dtype* bottom_diff = bottom[0]->mutable_cpu_diff();
+    const Dtype* prob_data = prob_.cpu_data();
+    caffe_copy(prob_.count(), prob_data, bottom_diff);
+    const Dtype* label = bottom[1]->cpu_data();
+    int dim = prob_.count() / outer_num_;
+    int count = 0;
+    for (int i = 0; i < outer_num_; ++i) {
+      for (int j = 0; j < inner_num_; ++j) {
+        const int label_value = static_cast<int>(label[i * inner_num_ + j]);
+        if (has_ignore_label_ && label_value == ignore_label_) {
+          for (int c = 0; c < bottom[0]->shape(softmax_axis_); ++c) {
+            bottom_diff[i * dim + c * inner_num_ + j] = 0;
+          }
+        } else {
+          bottom_diff[i * dim + label_value * inner_num_ + j] -= 1;
+          ++count;
+        }
+      }
+    }
+    // Scale gradient
+    const Dtype loss_weight = top[0]->cpu_diff()[0];
+    if (normalize_) {
+      caffe_scal(prob_.count(), loss_weight / count, bottom_diff);
+    } else {
+      caffe_scal(prob_.count(), loss_weight / outer_num_, bottom_diff);
+    }
+  }
+  /*if (propagate_down[1]) {
         LOG(FATAL) << this->type()
                    << " Layer cannot backpropagate to label inputs.";
     }
@@ -85,7 +145,7 @@ void SoftmaxWithLossLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>& top,
         } else {
             caffe_gpu_scal(prob_.count(), loss_weight / outer_num_, bottom_diff);
         }
-    }
+    }*/
 }
 
 INSTANTIATE_LAYER_GPU_FUNCS(SoftmaxWithLossLayer);
