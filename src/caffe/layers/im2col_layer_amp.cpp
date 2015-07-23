5dd2c4f672f40bcfea608bc605374e084cbc9b72
Yang Zhang <yangzhang@multicorewareinc.com>
2015-07-23 17:48:54 +0800

    Format code in im2col layer.

--- src/caffe/layers/im2col_layer_amp.cpp
+++ src/caffe/layers/im2col_layer_amp.cpp
@@ -1,5 +1,4 @@
 #include <vector>
-
 #include "caffe/common.hpp"
 #include "caffe/layer.hpp"
 #include "caffe/util/im2col.hpp"
@@ -14,13 +13,11 @@ void Im2colLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom,
   const Dtype* bottom_data = bottom[0]->gpu_data();
   Dtype* top_data = top[0]->mutable_gpu_data();
   for (int n = 0; n < bottom[0]->num(); ++n) {
-    im2col_gpu2(bottom[0]->count()-bottom[0]->offset(n),top[0]->count()-top[0]->offset(n),bottom_data + bottom[0]->offset(n), channels_, height_,
+    im2col_gpu2(bottom[0]->count()-bottom[0]->offset(n),
+        top[0]->count()-top[0]->offset(n),
+        bottom_data + bottom[0]->offset(n), channels_, height_,
         width_, kernel_h_, kernel_w_, pad_h_, pad_w_,
         stride_h_, stride_w_, top_data + top[0]->offset(n));
-    /*im2col_gpu(bottom_data + bottom[0]->offset(n), channels_, height_,
-        width_, kernel_h_, kernel_w_, pad_h_, pad_w_,
-        stride_h_, stride_w_, top_data + top[0]->offset(n));*/
- 
   }
 }
 
@@ -30,10 +27,9 @@ void Im2colLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>& top,
   const Dtype* top_diff = top[0]->gpu_diff();
   Dtype* bottom_diff = bottom[0]->mutable_gpu_diff();
   for (int n = 0; n < top[0]->num(); ++n) {
-    /*col2im_gpu(top_diff + top[0]->offset(n), channels_, height_, width_,
-        kernel_h_, kernel_w_, pad_h_, pad_w_,
-        stride_h_, stride_w_, bottom_diff + bottom[0]->offset(n));*/
-    col2im_gpu2(top[0]->count()-top[0]->offset(n),bottom[0]->count()-bottom[0]->offset(n),top_diff + top[0]->offset(n), channels_, height_, width_,
+    col2im_gpu2(top[0]->count()-top[0]->offset(n),
+        bottom[0]->count()-bottom[0]->offset(n),
+        top_diff + top[0]->offset(n), channels_, height_, width_,
         kernel_h_, kernel_w_, pad_h_, pad_w_,
         stride_h_, stride_w_, bottom_diff + bottom[0]->offset(n));
   }
@@ -44,4 +40,4 @@ INSTANTIATE_LAYER_GPU_FUNCS(Im2colLayer);
 
 }  // namespace caffe
 
-#endif  //USE_CPPAMP
+#endif  // USE_CPPAMP
