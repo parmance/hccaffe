c2ebbd1855ef1442795f8a8a05aa9dbbc2d3233b
Yang Zhang <yangzhang@multicorewareinc.com>
2015-07-23 17:02:46 +0800

    Format code in threshold layer.

--- src/caffe/layers/threshold_layer_amp.cpp
+++ src/caffe/layers/threshold_layer_amp.cpp
@@ -6,22 +6,20 @@
 #ifdef USE_CPPAMP
 template <typename Dtype>
 void ThresholdForwardKernel(const int N, Dtype threshold,
-	Dtype* in, Dtype* out);
+  Dtype* in, Dtype* out);
 namespace caffe {
 
 template <typename Dtype>
 void ThresholdLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom,
                                         const vector<Blob<Dtype>*>& top) {
-    Dtype* bottom_data = const_cast <Dtype*>(bottom[0]->gpu_data());
-    Dtype* top_data = top[0]->mutable_gpu_data();
-    const int count = bottom[0]->count();
-    // NOLINT_NEXT_LINE(whitespace/operators)
-	ThresholdForwardKernel(count, threshold_, bottom_data, top_data);
+  Dtype* bottom_data = const_cast <Dtype*>(bottom[0]->gpu_data());
+  Dtype* top_data = top[0]->mutable_gpu_data();
+  const int count = bottom[0]->count();
+  // NOLINT_NEXT_LINE(whitespace/operators)
+  ThresholdForwardKernel(count, threshold_, bottom_data, top_data);
 }
 
-
 INSTANTIATE_LAYER_GPU_FORWARD(ThresholdLayer);
-
 
 }  // namespace caffe
 #endif
