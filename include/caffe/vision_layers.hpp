13d30468c98f88453da60107fbfb51ea387b0e4d
Yang Zhang <yangzhang@multicorewareinc.com>
2015-08-19 10:33:25 +0800

    Modify vision_layers.hpp to use new memory frame

--- include/caffe/vision_layers.hpp
+++ include/caffe/vision_layers.hpp
@@ -106,19 +106,18 @@ class BaseConvolutionLayer : public Layer<Dtype> {
 #ifdef USE_CPPAMP
   inline void conv_im2col_gpu2(int N1, int N2, const Dtype* data,
       Dtype* col_buff) {
-    im2col_gpu2(N1, N2, data, 0,
-        conv_in_channels_,
+    im2col_gpu2(data, conv_in_channels_,
         conv_in_height_, conv_in_width_,
         kernel_h_, kernel_w_,
         pad_h_, pad_w_,
         stride_h_, stride_w_,
-        col_buff, 0);
+        col_buff, 0, 0);
   }
   inline void conv_col2im_gpu2(int N1, int N2, const Dtype* col_buff,
       Dtype* data) {
-    col2im_gpu2(N1, N2, col_buff, conv_in_channels_, conv_in_height_,
+    col2im_gpu2(col_buff, conv_in_channels_, conv_in_height_,
         conv_in_width_, kernel_h_, kernel_w_, pad_h_, pad_w_, stride_h_,
-        stride_w_, data);
+        stride_w_, data, 0, 0);
   }
 #endif  // USE_CPPAMP
 #endif  // CPU_ONLY
