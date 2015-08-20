43e0e129b35295fecd8de83723e54531f234b545
Yang Zhang <yangzhang@multicorewareinc.com>
2015-08-20 18:31:48 +0800

    Modify base data layer to use new memory architecture.

--- src/caffe/layers/base_data_layer_amp.cpp
+++ src/caffe/layers/base_data_layer_amp.cpp
@@ -1,3 +1,4 @@
+#include <boost/type_traits/is_same.hpp>
 #include <vector>
 
 #include "caffe/data_layers.hpp"
@@ -14,12 +15,23 @@ void BasePrefetchingDataLayer<Dtype>::Forward_gpu(
   top[0]->Reshape(this->prefetch_data_.num(), this->prefetch_data_.channels(),
       this->prefetch_data_.height(), this->prefetch_data_.width());
   // Copy the data
+#ifdef USE_CPPAMP
+  caffe_amp_H2D((void*)prefetch_data_.cpu_data(), (void*)top[0]->mutable_gpu_data(),
+      sizeof(Dtype), boost::is_same<Dtype, int>::value);
+  if (this->output_labels_) {
+    caffe_amp_H2D((void*)prefetch_label_.cpu_data(),
+        (void*)top[1]->mutable_gpu_data(),
+        sizeof(Dtype),
+        boost::is_same<Dtype, int>::value);
+  }
+#else
   caffe_copy(prefetch_data_.count(), prefetch_data_.cpu_data(),
       top[0]->mutable_gpu_data());
   if (this->output_labels_) {
     caffe_copy(prefetch_label_.count(), prefetch_label_.cpu_data(),
         top[1]->mutable_gpu_data());
   }
+#endif  // USE_CPPAMP
   // Start a new prefetch thread
   CreatePrefetchThread();
 }
