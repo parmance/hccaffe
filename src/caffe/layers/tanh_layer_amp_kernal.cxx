c5942b22d5fb27cec9c1bbd623a8feac22c5683f
xukai@multicorewareinc.com <xukai@multicorewareinc.com>
2015-08-26 10:58:41 +0800

    update tanh_layer

--- src/caffe/layers/tanh_layer_amp_kernal.cxx
+++ src/caffe/layers/tanh_layer_amp_kernal.cxx
@@ -21,64 +21,64 @@ void TanHBackward(const int N, Dtype* in_diff,
 
 template <>
 void TanHForward(const int N, float* in, float* out) {
-  array_view<float, 1> inView(N, in);
-  array_view<float, 1> outView(N, out);
+  array_view<float, 1> inView = *((Concurrency::array_view<float, 1>*)(in));
+  array_view<float, 1> outView = *((Concurrency::array_view<float, 1>*)(out));
+  Concurrency::extent<1> e(N);
   parallel_for_each(
-    outView.get_extent(),
+    e,
     [=](index<1> idx) restrict(amp)
   {
     outView[idx] = Concurrency::fast_math::tanh(inView[idx]);
   }
   );
-  outView.synchronize();
 }
 
 template <>
 void TanHBackward(const int N, float* in_diff,
   float* out_data, float* out_diff) {
-  array_view<float, 1> in_diffView(N, in_diff);
-  array_view<float, 1> out_diffView(N, out_diff);
-  array_view<float, 1> out_dataView(N, out_data);
+  array_view<float, 1> in_diffView = *((Concurrency::array_view<float, 1>*)(in_diff));
+  array_view<float, 1> out_diffView = *((Concurrency::array_view<float, 1>*)(out_diff));
+  array_view<float, 1> out_dataView = *((Concurrency::array_view<float, 1>*)(out_data));
+  Concurrency::extent<1> e(N);
   parallel_for_each(
-    out_dataView.get_extent(),
+    e,
     [=](index<1> idx) restrict(amp)
   {
     float tanhx = out_dataView[idx];
     out_diffView[idx] = in_diffView[idx] * (1 - tanhx * tanhx);
   }
   );
-  out_diffView.synchronize();
 }
 
 template <>
 void TanHForward(const int N, double* in, double* out) {
-  array_view<double, 1> inView(N, in);
-  array_view<double, 1> outView(N, out);
+  array_view<double, 1> inView = *((Concurrency::array_view<double, 1>*)(in));
+  array_view<double, 1> outView = *((Concurrency::array_view<double, 1>*)(out));
+  Concurrency::extent<1> e(N);
   parallel_for_each(
-    outView.get_extent(),
+    e,
     [=](index<1> idx) restrict(amp)
   {
     outView[idx] = Concurrency::fast_math::tanh(inView[idx]);
   }
   );
-  outView.synchronize();
 }
 
 template <>
 void TanHBackward(const int N, double* in_diff,
   double* out_data, double* out_diff) {
-  array_view<double, 1> in_diffView(N, in_diff);
-  array_view<double, 1> out_diffView(N, out_diff);
-  array_view<double, 1> out_dataView(N, out_data);
+  array_view<double, 1> in_diffView = *((Concurrency::array_view<double, 1>*)(in_diff));
+  array_view<double, 1> out_diffView = *((Concurrency::array_view<double, 1>*)(out_diff));
+  array_view<double, 1> out_dataView = *((Concurrency::array_view<double, 1>*)(out_data));
+  Concurrency::extent<1> e(N);
   parallel_for_each(
-    out_dataView.get_extent(),
+    e,
     [=](index<1> idx) restrict(amp)
   {
     double tanhx = out_dataView[idx];
     out_diffView[idx] = in_diffView[idx] * (1 - tanhx * tanhx);
   }
   );
-  out_diffView.synchronize();
 }
 
 
